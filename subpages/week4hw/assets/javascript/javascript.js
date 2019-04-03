7423fb682c4aeee84da3363dc6c1cebce8924019
Rich <rjw12498@gmail.com>
2019-04-02 21:24:59 -0600

    added week 4 homework

--- subpages/week4hw/assets/javascript/javascript.js
+++ subpages/week4hw/assets/javascript/javascript.js
@@ -1,8 +1,8 @@
 //need 4 variables to each crystal, each with a random value between 1-12
-var kirbyOGsc;
-var kirbyFatsc;
-var kirbyRunsc;
-var kirbySucksc;
+var kirbyOGsc = 1;
+var kirbyFatsc = 2;
+var kirbySucksc = 3;
+var kirbyRunsc = 4;
 
 //Possible image set to a variable
 
@@ -19,13 +19,81 @@ var losses = 0;
 //Goal is to click on the crystals until you match the goal
 //If you are under nothing happens, If you are over you lose, if it is exact you win
 //Needs to have a win state that resets itself after you win
+function gameLogic (){
+    if (userPoints === pointGoal){
+        alert("You win!")
+        wins += 1
+        //**********Re random point goal and kirby values */
+        userPoints = 0
+        randomer();
+    }
+    if (userPoints > pointGoal){
+        alert("You lost!")
+        losses += 1;
+        userPoints = 0;
+        randomer();
+    }
+}
+
+function randomer (){
+    //GOAL
+    pointGoal = Math.floor(Math.random() *100 + 20)
+    alert(pointGoal)
+    //Kirby
+    kirbyOGsc = Math.floor(Math.random() * 11 + 1)
+    kirbyFatsc = Math.floor(Math.random() * 11 + 1)
+    kirbySucksc = Math.floor(Math.random() * 11 + 1)
+    kirbyRunsc = Math.floor(Math.random() * 11 + 1)
+
+    //Resets the dom
+    $("#user-score").html(userPoints);
+    $("#wins").html(wins)
+    $("#losses").html(losses)
+    $("#goal").html(pointGoal)
+}
+
 $(document).ready(function(){
-alert("Document is ready!")
-console.log("Test")
-//Click the image!
+
+    $("#wins").html(wins)
+    $("#losses").html(losses)
+
+//DOM INTERACTION and Stuff that needs to be reset in event of a new game
+
+$("#goal").html(pointGoal)
+ //Set a randomizer to redefine variables   
+
+
+
+//MAkes the images interactable. Selects the images and adds points when they are clicked, as well as updating the user score.
+
 $("#kirby-og").click(function (){
-alert("POYO")
+
+    userPoints += kirbyOGsc;
+    $("#user-score").html(userPoints);
+    gameLogic();
+   
+    
 })
 
+$("#kirby-fat").click(function (){
+ 
+    userPoints += kirbyFatsc;
+    $("#user-score").html(userPoints);
+    gameLogic();
+   
+})
+
+$("#kirby-run").click(function (){
+    
+    userPoints += kirbyRunsc;
+    $("#user-score").html(userPoints);
+    gameLogic();
+})
+
+$("#kirby-suck").click(function (){
+    userPoints += kirbySucksc;
+    $("#user-score").html(userPoints);
+   gameLogic();
+})
 })
 
