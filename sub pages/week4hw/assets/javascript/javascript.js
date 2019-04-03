7eecda92292b306b882b95c2b1d5e381d3fbdb9a
Rich <rjw12498@gmail.com>
2019-04-02 19:51:56 -0600

    added old homework

---
+++ sub pages/week4hw/assets/javascript/javascript.js
@@ -0,0 +1,31 @@
+//need 4 variables to each crystal, each with a random value between 1-12
+var kirbyOGsc;
+var kirbyFatsc;
+var kirbyRunsc;
+var kirbySucksc;
+
+//Possible image set to a variable
+
+
+//Need a two more variables, one that is the goal (******THE GOAL (between 19 and 120) IS RANDOMLY SELECTED******) and one that holds the users current amount of points
+var pointGoal = 21;
+var userPoints = 0;
+
+//Counter for wins and losses
+var wins = 0;
+var losses = 0;
+
+//Logic for game
+//Goal is to click on the crystals until you match the goal
+//If you are under nothing happens, If you are over you lose, if it is exact you win
+//Needs to have a win state that resets itself after you win
+$(document).ready(function(){
+alert("Document is ready!")
+console.log("Test")
+//Click the image!
+$("#kirby-og").click(function (){
+alert("POYO")
+})
+
+})
+
